665be94d5bfede3fb6950371d9670c37ed070162
Dmytro Zaitsev <dmytro.zaitsev@piano.io>
2024-05-19 11:36:02 +0300

    Enable CORS

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const corsOptions: CorsOptions = {
+    origin: [
+      'http://localhost:4200',
+      'http://wedding-12-09.s3-website.eu-north-1.amazonaws.com',
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  };
+
+  app.enableCors(corsOptions);
+
   await app.listen(3000);
 }
 bootstrap();
